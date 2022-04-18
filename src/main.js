4530cc9feff3c119e5f13c18235b49b84ee0bf2b
Niccolo Abate (Chromebook) <nlabate@ucsc.edu>
2022-04-17 20:44:15 -0700

    Updated comment header

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 /*
-## Points Breakdown
-Simultaneous 2 player mode - 60
-Code Refactoring using JS techniques - 5
+### Points Breakdown - More Info in README
+
+### Simultaneous 2 player mode - 30
+### Mode Selection and differences between single player and 2 player * - 5
+### Add time to the clock on hits (Single Player Only) - 20
+### Paralax Scrolling - 10
+### SFX Randomness / Variation - 10
+### Code Refactoring using JS techniques * - 5
+### Play Scene Music - 5
+### Music Interactivity * - 25
+### TOTAL - 110
+
+'*' = Custom / proposed additions
 */
 
 let config = {
